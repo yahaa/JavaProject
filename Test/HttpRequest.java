f1726bd92ad846fdab5048a72015960651343a87
yahaa <1477765176@qq.com>
2016-10-12 10:17:13 +0800

    ettercap

--- Test/HttpRequest.java
+++ Test/HttpRequest.java
@@ -9,61 +9,9 @@ import java.util.Map;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.io.OutputStreamWriter;
+
 public class HttpRequest {
     
-    final static String url = "";  
-    final static String params = "{\"id\":\"12345\"}";  
-  
-    /** 
-     * 发送HttpPost请求 
-     *  
-     * @param strURL 
-     *            服务地址 
-     * @param params 
-     *            json字符串,例如: "{ \"id\":\"12345\" }" ;其中属性名必须带双引号<br/> 
-     * @return 成功:返回json字符串<br/> 
-     */  
-    public static String post(String strURL, String params) {  
-        System.out.println(strURL);  
-        System.out.println(params);  
-        try {  
-            URL url = new URL(strURL);// 创建连接  
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();  
-            connection.setDoOutput(true);  
-            connection.setDoInput(true);  
-            connection.setUseCaches(false);  
-            connection.setInstanceFollowRedirects(true);  
-            connection.setRequestMethod("POST"); // 设置请求方式  
-            connection.setRequestProperty("Accept", "application/json"); // 设置接收数据的格式  
-            connection.setRequestProperty("Content-Type", "application/json"); // 设置发送数据的格式  
-            connection.connect();  
-            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8"); // utf-8编码  
-            out.append(params);  
-            out.flush();  
-            out.close();  
-            // 读取响应  
-            int length = (int) connection.getContentLength();// 获取长度  
-            InputStream is = connection.getInputStream();  
-            if (length != -1) {  
-                byte[] data = new byte[length];  
-                byte[] temp = new byte[512];  
-                int readLen = 0;  
-                int destPos = 0;  
-                while ((readLen = is.read(temp)) > 0) {  
-                    System.arraycopy(temp, 0, data, destPos, readLen);  
-                    destPos += readLen;  
-                }  
-                String result = new String(data, "UTF-8"); // utf-8编码  
-                System.out.println(result);  
-                return result;  
-            }  
-        } catch (IOException e) {  
-            // TODO Auto-generated catch block  
-            e.printStackTrace();  
-        }  
-        return "error"; // 自定义错误信息  
-    }  
-
     /**
      * 向指定 URL 发送POST方法的请求
      * 
@@ -84,8 +32,7 @@ public class HttpRequest {
             // 设置通用的请求属性
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
